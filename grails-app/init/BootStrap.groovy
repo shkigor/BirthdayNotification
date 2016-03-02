2543703fbac6ab1774fa9a5292523bcbd71fb2b0
Igor Shkliar <igorsh@default-value.com>
2016-03-02 23:53:12 +0200

    add simple bootstrap data

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,17 @@
+import ck.solo.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!User.count()) createSampleData()
     }
     def destroy = {
+    }
+
+    private void createSampleData() {
+        User igor = new User(lastName: 'Шкляр', firstName: 'Игорь', birthday: new Date().parse('dd.MM.yyyy', '11.12.1978'), dateCreated: new Date('02/02/2016'), dateUpdated: new Date('02/02/2016'), bdate: 11, bmonth: 12, byear: 1978).save(failOnError: true)
+        User inna = new User(lastName: 'Шкляр', firstName: 'Инна', birthday: new Date().parse('dd.MM.yyyy', '23.04.1985'), dateCreated: new Date('02/02/2016'), dateUpdated: new Date('02/02/2016'), bdate: 23, bmonth: 04, byear: 1985).save(failOnError: true)
+        User user01 = new User(lastName: 'Иванов', firstName: 'Иван', birthday: new Date('03/02/1985'), dateCreated: new Date('02/02/2016'), dateUpdated: new Date('02/02/2016'), bdate: 3, bmonth: 2, byear: 1985).save(failOnError: true)
+        User user02 = new User(lastName: 'Сидоров', firstName: 'Степан', birthday: new Date('03/02/1980'), dateCreated: new Date('02/02/2016'), dateUpdated: new Date('02/02/2016'), bdate: 3, bmonth: 2, byear: 1985).save(failOnError: true)
     }
 }
